a7f8b2b339d90e34ea6f7db04279cdec5e76b807
Patryk Zawadzki <patrys@pld-linux.org>
2008-03-05 14:35:47 +0100

    - add comments for newly introduced methods

--- includes/database.php
+++ includes/database.php
@@ -2,40 +2,42 @@
 	class QueryBuilder {
 		/**
 		 * Function: build_update_values
-		 * Creates an update data part
+		 * Creates an update data part.
 		 */
 		public static function build_update_values($data) {
 			$set = array();
-
+			
 			foreach ($data as $field => $val)
-			{
 				array_push($set, "`$field` = $val");
-			}
-
-			return implode(', ', $set);
+			
+			return implode(", ", $set);
 		}
-
+		
 		/**
 		 * Function: build_insert_header
-		 * Creates an insert header part
+		 * Creates an insert header part.
 		 */
 		public static function build_insert_header($data) {
 			$set = array();
-
+			
 			foreach (array_keys($data) as $field)
 				array_push($set, "`$field`");
-
-			return '(' . implode(', ', $set) . ')';
+			
+			return "(".implode(", ", $set).")";
 		}
-
+		
 		/**
 		 * Function: build_insert_values
-		 * Creates an insert data part
+		 * Creates an insert data part.
 		 */
 		public static function build_insert_values($data) {
-			return '(' . implode(', ', array_values($data)) . ')';
+			return "(".implode(', ', array_values($data)).")";
 		}
-
+		
+		/**
+		 * Function: build_insert
+		 * Creates a full insert query.
+		 */
 		public static function build_insert($table, $data) {
 			return "
 				INSERT INTO `$table`
@@ -44,7 +46,11 @@
 				".self::build_insert_values($data)."
 			";
 		}
-
+		
+		/**
+		 * Function: build_update
+		 * Creates a full update query.
+		 */
 		public static function build_update($table, $conds, $data) {
 			return "
 				UPDATE `$table`
@@ -52,7 +58,11 @@
 				".($conds ? "WHERE $conds" : "")."
 			";
 		}
-
+		
+		/**
+		 * Function: build_limits
+		 * Creates a LIMIT part for a query.
+		 */
 		public static function build_limits($offset, $limit) {
 			if ($limit === null)
 				return "";
@@ -60,7 +70,12 @@
 				return "LIMIT $offset, $limit";
 			return "LIMIT $limit";
 		}
-
+		
+		/**
+		 * Function: build_from
+		 * Creates a FROM header for select queries.
+		 * Table names are automatically prefixed.
+		 */
 		public static function build_from($tables) {
 			if (!is_array($tables))
 				$tables = array($tables);
@@ -77,7 +92,11 @@
 			}
 			return implode(", ", $set);
 		}
-
+		
+		/**
+		 * Function: build_count
+		 * Creates a SELECT COUNT(1) query.
+		 */
 		public static function build_count($tables, $conds) {
 			return "
 				SELECT COUNT(1) AS count
@@ -85,13 +104,16 @@
 				".($conds ? "WHERE $conds" : "")."
 			";
 		}
-
+		
+		/**
+		 * Function: build_select_header
+		 * Creates a SELECT fields header.
+		 */
 		public static function build_select_header($fields) {
 			if (!is_array($fields))
 				$fields = array($fields);
 			$set = array();
-
-			$sql = SQL::current();
+			
 			foreach ($fields as $field) {
 				$field = explode(" ", $field);
 				$parts = explode(".", $field[0]);
@@ -101,9 +123,14 @@
 				$field[0] = implode(".", $parts);
 				array_push($set, implode(" ", $field));
 			}
-
+			
 			return implode(', ', $set);
 		}
+		
+		/**
+		 * Function: build_select
+		 * Creates a full SELECT query.
+		 */
 		public static function build_select($tables, $fields, $conds, $order = null, $limit = null, $offset = null) {
 			return "
 				SELECT ".self::build_select_header($fields)."
@@ -121,6 +148,7 @@
 	 */
 	class SQL {
 		/**
+		 * Function: __construct
 		 * The class constructor is private so there is only one connection.
 		 */
 		private function __construct() {
@@ -219,11 +247,19 @@
 			return $q;
 		}
 		
+		/**
+		 * Function: count
+		 * Performs a counting query and returns the number of matching rows.
+		 */
 		public function count($tables, $conds, $params = array())
 		{
 			return $this->query(QueryBuilder::build_count($tables, $conds), $params)->fetchColumn();
 		}
 		
+		/**
+		 * Function: select
+		 * Performs a SELECT with given criteria and returns the query result object.
+		 */
 		public function select($tables, $fields, $conds, $order = null, $params = array(), $limit = null, $offset = null)
 		{
 			return $this->query(QueryBuilder::build_select($tables, $fields, $conds, $order, $limit, $offset), $params);
