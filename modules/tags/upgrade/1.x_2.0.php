6b275ddc4e5dc54c5994b95797740232349812ed
Alex Suraci <i.am@toogeneric.com>
2008-06-12 13:31:35 -0400

    Updated Tags module upgrader.

--- modules/tags/upgrade/1.x_2.0.php
+++ modules/tags/upgrade/1.x_2.0.php
@@ -1,20 +1,33 @@
 <?php
-	require "../../includes/common.php";
+	require "../../../includes/common.php";
 
 	# Back up all the current tags.
+	echo "If the upgrade fails, here's a backup:<br />\n";
+	echo '<textarea rows="15" cols="100">';
+	echo "CREATE TABLE `".$sql->prefix."tags` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(250) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `clean` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) DEFAULT CHARSET=utf8;\n\n";
 	$tags = array();
 	foreach ($sql->select("tags", "*", null)->fetchAll() as $tag) {
 		if (!isset($tags[$tag["post_id"]]))
 			$tags[$tag["post_id"]] = array("normal" => array(), "clean" => array());
 
+		echo "INSERT INTO `".$sql->prefix."tags` VALUES (`post_id`, `name`, `clean`) (".$tag["post_id"].", '".$tag["name"]."', '".$tag["clean"]."')\n";
 		$tags[$tag["post_id"]]["normal"][] = "{{".$tag["name"]."}}";
 		$tags[$tag["post_id"]]["clean"][] = "{{".$tag["clean"]."}}";
 	}
+	echo "</textarea>\n<br /><br />\n";
 
 	# Drop the old table.
+	echo "Dropping current Tags database table...<br /><br />\n";
 	$sql->query("DROP TABLE `__tags`");
 
 	# Create the new table.
+	echo "Creating new database table...<br /><br />\n";
 	$sql->query("CREATE TABLE IF NOT EXISTS `__tags` (
 			      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
 			      `tags` VARCHAR(250) DEFAULT '',
@@ -23,8 +36,11 @@
 			     ) DEFAULT CHARSET=utf8");
 
 	# Create the tags in the new format.
+	echo "Inserting new tags... Executing the following:<br />\n";
+	echo '<textarea rows="15" cols="100">';
 	foreach ($tags as $post => $tag) {
-		$sql->insert("__tags",
+		echo "INSERT INTO `".$sql->prefix."tags` VALUES (`tags`, `clean`, `post_id`) ('".implode(" ", $tag["normal"])."', '".implode(" ", $tag["clean"])."', ".$post.")\n";
+		$sql->insert("tags",
 		             array("tags" => ":tags",
 		                   "clean" => ":clean",
 		                   "post_id" => ":post_id"),
@@ -32,6 +48,7 @@
 		                  ":clean" => implode(" ", $tag["clean"]),
 		                  ":post_id" => $post));
 	}
+	echo "</textarea>\n<br /><br />\n";
 
 	echo "Done!";
 ?>
