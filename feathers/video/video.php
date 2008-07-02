12c9e75d60b829b73b0e3335612103ad360e3710
Alex Suraci <i.am@toogeneric.com>
2008-07-01 21:54:00 -0400

    Slight adjustment to youtube detecting.

--- feathers/video/video.php
+++ feathers/video/video.php
@@ -5,7 +5,7 @@
 			                      "type" => "text_block",
 			                      "rows" => 4,
 			                      "label" => __("Video", "video"),
-			                      "bookmarklet" => (isset($_GET['url']) and strpos($_GET['url'], "youtube")) ? "url" : ""));
+			                      "bookmarklet" => (isset($_GET['url']) and strpos($_GET['url'], ".youtube.")) ? "url" : ""));
 			$this->setField(array("attr" => "caption",
 			                      "type" => "text_block",
 			                      "rows" => 4,
