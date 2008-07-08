81227567cc3915b080b4faf551c0831ea22096d4
Alex Suraci <i.am@toogeneric.com>
2008-07-07 20:14:42 -0400

    Fixed the error display sometimes not having Config available.

--- includes/error.php
+++ includes/error.php
@@ -8,7 +8,11 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>Chyrp: <?php echo $title; ?></title>
+<?php if (class_exists("Config")): ?>
 		<script src="<?php echo Config::current()->chyrp_url; ?>/includes/lib/gz.php?file=jquery.js" type="text/javascript" charset="utf-8"></script>
+<?php else: ?>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<?php endif; ?>
 		<style type="text/css">
 			html, body, ul, ol, li,
 			h1, h2, h3, h4, h5, h6,
