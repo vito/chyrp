1203fa4e51119f191077cfe378f5dce80f78faae
Alex Suraci <i.am@toogeneric.com>
2008-08-17 01:43:08 -0400

    Increased flexibility of the SWFUpload module. [#176 state:resolved]

--- modules/swfupload/swfupload.php
+++ modules/swfupload/swfupload.php
@@ -14,25 +14,44 @@
 				echo '		<link rel="stylesheet" href="'.$config->chyrp_url.'/modules/swfupload/style.css" type="text/css" media="screen" title="no title" charset="utf-8" />'."\n";
 				echo '		<script type="text/javascript">'."\n";
 				echo "			$(function(){\n";
-				foreach ($this->insert_swfupload as $id => $extensions) {
+				foreach ($this->insert_swfupload as $id => $options) {
+					$upload_url                   = $config->chyrp_url."/modules/swfupload/upload_handler.php";
+					$flash_url                    = $config->chyrp_url."/modules/swfupload/lib/FlashDevelop/Flash9/swfupload_f9.swf";
+					$file_types                   = "*";
+					$file_types_description       = "All Files";
+					$debug                        = false;
+					$file_queue_error_handler     = "fileQueueError";
+					$file_dialog_complete_handler = "fileDialogComplete";
+					$upload_start_handler         = "uploadStart";
+					$upload_progress_handler      = "uploadProgress";
+					$upload_error_handler         = "uploadError";
+					$upload_success_handler       = "uploadSuccess";
+					$upload_complete_handler      = "uploadComplete";
+
+					if (is_string($options))
+						$file_types = $options;
+					else
+						foreach ($options as $key => $val)
+							$$key = $val;
+
 					echo '				$("#'.$id.'").replaceWith("<input type=\"button\" value=\"Upload\" class=\"swfupload_button\" id=\"'.$id.'\" />")'."\n";
 					echo "				".$id." = new SWFUpload({\n";
-					echo '					upload_url : "'.$config->chyrp_url.'/modules/swfupload/upload_handler.php",'."\n";
-					echo '					flash_url : "'.$config->chyrp_url.'/modules/swfupload/lib/FlashDevelop/Flash9/swfupload_f9.swf",'."\n";
+					echo '					upload_url : "'.$upload_url.'",'."\n";
+					echo '					flash_url : "'.$flash_url.'",'."\n";
 					echo '					post_params: {"PHPSESSID" : "'.session_id().'", "PHPSESSNAME" : "'.session_name().'", "ajax" : "true" },'."\n";
 					echo '					file_size_limit : "100 MB",'."\n";
-					echo '					file_types : "'.$extensions.'",'."\n";
-					echo '					file_types_description : "All Files",'."\n";
-					#echo '					debug: true,'."\n";
-                    echo '					'."\n";
-					echo '					// The event handler functions are defined in handlers.js'."\n";
-					echo '					file_queue_error_handler : fileQueueError,'."\n";
-					echo '					file_dialog_complete_handler : fileDialogComplete,'."\n";
-					echo '					upload_start_handler : uploadStart,'."\n";
-					echo '					upload_progress_handler : uploadProgress,'."\n";
-					echo '					upload_error_handler : uploadError,'."\n";
-					echo '					upload_success_handler : uploadSuccess,'."\n";
-					echo '					upload_complete_handler : uploadComplete'."\n";
+					echo '					file_types : "'.$file_types.'",'."\n";
+					echo '					file_types_description : "'.$file_types_description.'",'."\n";
+					if ($debug)
+						echo '					debug: true,'."\n";
+					echo '					'."\n";
+					echo '					file_queue_error_handler : '.$file_queue_error_handler.','."\n";
+					echo '					file_dialog_complete_handler : '.$file_dialog_complete_handler.','."\n";
+					echo '					upload_start_handler : '.$upload_start_handler.','."\n";
+					echo '					upload_progress_handler : '.$upload_progress_handler.','."\n";
+					echo '					upload_error_handler : '.$upload_error_handler.','."\n";
+					echo '					upload_success_handler : '.$upload_success_handler.','."\n";
+					echo '					upload_complete_handler : '.$upload_complete_handler.''."\n";
 					echo '				})'."\n";
 
 					echo '				$("#'.$id.'").click(function(){'."\n";
@@ -41,22 +60,6 @@
 				}
 				echo "			})\n";
 				echo "		</script>\n";
-				echo '		<style type="text/css">'."\n";
-				echo '			#progress {'."\n";
-				echo '				background: #fff;'."\n";
-				echo '				padding: 1px;'."\n";
-				echo '				border: 1px solid #ddd;'."\n";
-				echo '				display: none;'."\n";
-				echo '			}'."\n";
-				echo '			#progress .back {'."\n";
-				echo '				background: #f0f0f0;'."\n";
-				echo '			}'."\n";
-				echo '			#progress .fill {'."\n";
-				echo '				background-color: #5fb904;'."\n";
-				echo '				height: 24px;'."\n";
-				echo '				width: 0;'."\n";
-				echo '			}'."\n";
-				echo '		</style>'."\n";
 			}
 		}
 
