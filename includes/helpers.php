f870b9f84e8ab081daa8fa80fe990259f2ab0746
Alex Suraci <i.am@toogeneric.com>
2008-10-24 18:40:29 -0400

    Fixed list_notate with single arrays.

--- includes/helpers.php
+++ includes/helpers.php
@@ -1373,7 +1373,7 @@
         $items = array();
         foreach ($array as $item) {
             $string = (is_string($item) and $quotes) ? "&#8220;".$item."&#8221;" : $item ;
-            if (count($array) == ++$count)
+            if (count($array) == ++$count and $count !== 1)
                 $items[] = __("and ").$string;
             else
                 $items[] = $string;
