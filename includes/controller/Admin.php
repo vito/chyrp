b87024eb617ef894df02b87b97c3d7a3608070d9
Alex Suraci <i.am@toogeneric.com>
2009-06-29 13:27:10 -0400

    Corrected Admin controller.

--- includes/controller/Admin.php
+++ includes/controller/Admin.php
@@ -29,7 +29,7 @@
          * Prepares Twig.
          */
         private function __construct() {
-            $this->twig = new Twig_Loader((file_exists(THEME_DIR."/admin") ? THEME_DIR."/admin" : MAIN_DIR."/admin/theme"),
+            $this->twig = new Twig_Loader((file_exists(THEME_DIR."/admin") ? THEME_DIR."/admin" : MAIN_DIR."/admin/layout"),
                                           (is_writable(INCLUDES_DIR."/caches") and !DEBUG) ?
                                               INCLUDES_DIR."/caches" :
                                               null);
@@ -2177,7 +2177,7 @@
          * Renders the page.
          *
          * Parameters:
-         *     $action - The template file to display, in /admin/theme/pages.
+         *     $action - The template file to display, in /admin/layout/pages.
          *     $context - Context for the template.
          *     $title - The title for the page. Defaults to a camlelization of the action, e.g. foo_bar -> Foo Bar.
          */
@@ -2275,9 +2275,9 @@
 
             $this->context["sql_debug"]  = SQL::current()->debug;
 
-            $template = file_exists(THEME_DIR."/admin/theme/pages/".$action.".twig") ?
+            $template = file_exists(THEME_DIR."/admin/layout/pages/".$action.".twig") ?
                             THEME_DIR."/admin/pages/".$action.".twig" :
-                            MAIN_DIR."/admin/theme/pages/".$action.".twig" ;
+                            MAIN_DIR."/admin/layout/pages/".$action.".twig" ;
 
             $config = Config::current();
             if (!file_exists($template)) {
