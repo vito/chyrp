2d89fddca0184fcf5e05466edc7f7eb11ca4a294
Alex Suraci <i.am@toogeneric.com>
2009-07-06 13:18:45 -0400

    * Fixed Tagginator providing invalid dirty URLs. * Fixed broken rename_tag often resulting in editing an "empty" tag. * Fixed invalid tag URLs from /admin. * Fixed links to the Extend areas in the installer and Extend admin   pages.

--- install.php
+++ install.php
@@ -611,9 +611,9 @@
 <?php if (!is_writable(INCLUDES_DIR."/caches")): ?>
                 <li><?php echo __("CHMOD <code>/includes/caches</code> to 777."); ?></li>
 <?php endif; ?>
-                <li><a href="http://chyrp.net/extend/browse/translations"><?php echo __("Look for a translation for your language."); ?></a></li>
-                <li><a href="http://chyrp.net/extend/browse/modules"><?php echo __("Install some Modules."); ?></a></li>
-                <li><a href="http://chyrp.net/extend/browse/feathers"><?php echo __("Find some Feathers you want."); ?></a></li>
+                <li><a href="http://chyrp.net/extend/type/translation"><?php echo __("Look for a translation for your language."); ?></a></li>
+                <li><a href="http://chyrp.net/extend/type/module"><?php echo __("Install some Modules."); ?></a></li>
+                <li><a href="http://chyrp.net/extend/type/feather"><?php echo __("Find some Feathers you want."); ?></a></li>
                 <li><a href="README.markdown"><?php echo __("Read &#8220;Getting Started&#8221;"); ?></a></li>
             </ol>
             <a class="big" href="<?php echo $config->chyrp_url; ?>"><?php echo __("Take me to my site! &rarr;"); ?></a>
