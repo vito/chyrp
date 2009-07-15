e795987e9a5cd9fb29998031c6725cb74a68d5c8
Alex Suraci <i.am@toogeneric.com>
2009-07-15 17:42:31 -0400

    Fixed gettext.rb incorrectly requiring the "theme" domain to be specified for plural-form translations. [#12 state:resolved]

--- gettext.rb
+++ gettext.rb
@@ -52,6 +52,7 @@ class Gettext
 
     @domain = (OPTIONS[:domain].nil?) ? "" : ', "'+OPTIONS[:domain]+'"'
     @twig_domain = (OPTIONS[:domain].nil? or OPTIONS[:domain] == "theme") ? "" : '\("'+OPTIONS[:domain]+'"\)'
+    @twig_arg_domain = (OPTIONS[:domain].nil? or OPTIONS[:domain] == "theme") ? "" : ', "'+OPTIONS[:domain]+'"'
 
     prepare_files
     do_scan
@@ -174,7 +175,7 @@ class Gettext
   end
 
   def scan_twig_plural(text, line, filename, clean_filename)
-    text.gsub(/[\s\{\(](#{STRING})\s*\|\s*translate_plural\((#{STRING}), .*?#{@domain}\)\s*\|\s*format\(.*?\)/) do
+    text.gsub(/[\s\{\(](#{STRING})\s*\|\s*translate_plural\((#{STRING}),.*?#{@twig_arg_domain}\)\s*\|\s*format\(.*?\)/) do
       if @translations[$1].nil?
         @translations[$1] = { :places => [clean_filename + ":" + line.to_s],
                               :filter => true,
