4f151f52800c9c07ddda8deaf96ceea144edb9f3
Igor Tarasov <tarasov.igor@gmail.com>
2011-06-25 14:26:08 +0400

    Fix a typo in previous commit.

--- includes/helpers.php
+++ includes/helpers.php
@@ -1088,7 +1088,7 @@
 
         if (ini_get("allow_url_fopen")) {
             $content = @file_get_contents($url);
-            if (!strpos($http_response_header[0], " 200 OK")
+            if (!strpos($http_response_header[0], " 200 OK"))
                 $content = "Server returned a message: $http_response_header[0]";
         } elseif (function_exists("curl_init")) {
             $handle = curl_init();
