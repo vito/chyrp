b0356354ed831ed836c989895f25c11539b74c82
Arian Xhezairi <arian@xhezairi.com>
2011-08-12 10:50:11 +0200

    Removed an unnecessary parameter passed to the truncate function in the Read More module.

--- modules/read_more/read_more.php
+++ modules/read_more/read_more.php
@@ -27,7 +27,7 @@
                 return preg_replace('/(<p>)?<a class="read_more" href="([^"]+)">e51b2b9a58824dd068d8777ec6e97e4d<\/a>\(\(\(more(\((.+)\))?\)\)\)(<\/p>(\n\n<\/p>(\n\n)?)?)?/', "", $text);
 
             preg_match_all("/e51b2b9a58824dd068d8777ec6e97e4d(\(\(\(more(\((.+)\))?\)\)\))/", preg_replace("/<[^>]+>/", "", $text), $more, PREG_OFFSET_CAPTURE);
-            $body = truncate($text, $more[1][0][1], "", true, true, true);
+            $body = truncate($text, $more[1][0][1], "", true, true);
             $body.= @$more[3][0];
 
             if (!empty($more[2][0]))
