e8145bc762f4d4b41aceb3a134e98a04df860546
jack <jack.lightbody@gmail.com>
2011-09-04 10:49:40 -0300

    bug fixes- email blog should be ready to go now.

--- modules/emailblog/emailblog.php
+++ modules/emailblog/emailblog.php
@@ -4,50 +4,58 @@
         	$config = Config::current();
         	$username = $config->email_blog_address;
         	$password = $config->email_blog_pass;
+       		// this isn't working well on localhost
         	if($username!="example@gmail.com"&&$password!="password"){
-           $this->addAlias("runtime", "getMail");
-           }
+                $this->addAlias("runtime", "getMail");
+            }
            //run on every page load- not very efficient, but it works.
         }
-        /* Gets the mail from the inbox
+        /** 
+        * Gets the mail from the inbox
         * Reads all the messages there, and adds posts based on them. Then it deletes the entire mailbox.
         */
-		function getMail(){
-       		$hostname = '{imap.gmail.com:993/ssl/novalidate-cert}INBOX';
-       		// this isn't working well on localhost
-       		$config = Config::current();
-        	$username = $config->email_blog_address;
-        	$password = $config->email_blog_pass;
-        	$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
-        	$emails = imap_search($inbox,'ALL');
-        	if($emails) {
-        	    rsort($emails);
-        	    foreach($emails as $email_number) {    
-        	        $message = imap_fetchbody($inbox,$email_number,2);  
-        	        $overview = imap_fetch_overview($inbox,$email_number,0);
-        	        //imap_delete($inbox,$email_number);
-        	        $title=htmlspecialchars($overview[0]->subject);
-        	        $body=htmlspecialchars($message);
-        	        Post::add(array("title" => $title,
-        	        "body" => $message));
-        	        //we set the subject of the message as the page title and body as the email content- not sure about compatibility with images or feathers.
-        	    }
-        	} 
-        	
-        	// close the connection 
-        	//imap_close($inbox,CL_EXPUNGE);
-		}
+	function getMail(){
+	    $config = Config::current();
+	    if(time()-3600>=$config->checkemailblog){
+       	        $hostname = '{imap.gmail.com:993/ssl/novalidate-cert}INBOX';
+       	        // this isn't working well on localhost
+                $username = $config->email_blog_address;
+                $password = $config->email_blog_pass;
+                $inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+                $emails = imap_search($inbox,'ALL');
+                if($emails) {
+                    rsort($emails);
+                    foreach($emails as $email_number) {    
+                        $message = imap_body($inbox,$email_number);  
+                        $overview = imap_headerinfo($inbox,$email_number);
+                        imap_delete($inbox,$email_number);
+                        $title=htmlspecialchars($overview->Subject);
+                        $body=htmlspecialchars($message);
+                        Post::add(array("title" => $title,
+                        "body" => $message),$title,$title,"text");
+                        //we set the subject of the message as the page title and body as the email content- not sure about compatibility with images or feathers.
+                    }
+                } 
+                
+                // close the connection 
+                imap_close($inbox, CL_EXPUNGE);
+                $config->set("checkemailblog", time());
+            }
+	}
+		// called on install- we init all the configs.
 		static function __install() {
             $config = Config::current();
             $config->set("email_blog_address", "example@gmail.com");
             $config->set("email_blog_pass", "password");
+            $config->set("checkemailblog", time());
         }
+        // called on uninstall- we delete all the configs.
         static function __uninstall($confirm) {
              $config = Config::current();
             $config->remove("email_blog_address");
             $config->remove("email_blog_pass");
         }
-		static function manage_nav_pages($pages) {
+	static function manage_nav_pages($pages) {
             array_push($pages, "email_blog_settings");
             return $pages;
         }
