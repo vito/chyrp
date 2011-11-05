412939bcb7729ddd00005aa344cdb7e0ffc641c7
Arian Xhezairi <arian@xhezairi.com>
2011-11-05 19:10:58 +0100

    Modified both markup modules to reflect the update changes.
    Applied Chyrp Code Styling.

--- includes/lib/markitup/markitup.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $("#write_form textarea, #edit_form textarea").markItUp(mySettings);
-});
-
--- modules/textilize/textilize.php
+++ modules/textilize/textilize.php
@@ -15,21 +15,22 @@
             if (!$config->enable_wysiwyg)
                 return;
 
-            $current_action = isset(Route::current()->action) ? Route::current()->action : 'write_post';
-            $wysiwyg_actions = array('write_post', 'edit_post', 'write_page', 'edit_page');
+            $current_action = isset(Route::current()->action) ? Route::current()->action : "write_post" ;
+            $wysiwyg_actions = array("write_post", "edit_post", "write_page", "edit_page");
             if (!in_array($current_action, $wysiwyg_actions))
                 return;
 
-            $path = $config->chyrp_url.'/includes/';
-
-            ?>
-
-            <link href="<?php echo $path; ?>lib/markitup/skins/simple/style.css" type="text/css" rel="stylesheet">
-            <link href="<?php echo $path; ?>lib/markitup/sets/textile/style.css" type="text/css" rel="stylesheet">
-            <script src="<?php echo $path; ?>lib/markitup/jquery.markitup.js" type="text/javascript" charset="utf-8"></script>
-            <script src="<?php echo $path; ?>lib/markitup/sets/textile/set.js" type="text/javascript" charset="utf-8"></script>
-            <script src="<?php echo $path; ?>lib/markitup/markitup.js" type="text/javascript" charset="utf-8"></script>
-    
-            <?php
+            $path = $config->chyrp_url."/includes/lib/markitup/";
+      ?>
+      <link href="<?php echo $path; ?>skins/simple/style.css" type="text/css" rel="stylesheet">
+      <link href="<?php echo $path; ?>sets/textile/style.css" type="text/css" rel="stylesheet">
+      <script src="<?php echo $path; ?>jquery.markitup.js" type="text/javascript" charset="utf-8"></script>
+      <script src="<?php echo $path; ?>sets/textile/set.js" type="text/javascript" charset="utf-8"></script>
+      <script type="text/javascript">
+        $(document).ready(function(){
+            $("#write_form textarea, #edit_form textarea").markItUp(mySettings);
+        });
+      </script>
+      <?php
         }
     }
