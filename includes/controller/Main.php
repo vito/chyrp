10f877e056bbbe28d7fbea830504624f9481327d
Arian Xhezairi <arian@xhezairi.com>
2012-02-06 13:53:04 +0100

    Fixed: Undefined index $SESSION['redirect_to'] upon login.

--- includes/controller/Main.php
+++ includes/controller/Main.php
@@ -580,8 +580,10 @@
                     $user = new User(array("login" => $_POST['login']));
                     $_SESSION['user_id'] = $user->id;
 
-                    $redirect = $_SESSION['redirect_to'];
-                    unset($_SESSION['redirect_to']);
+                    if (isset($_SESSION['redirect_to'])) {
+                        $redirect = $_SESSION['redirect_to'];
+                        unset($_SESSION['redirect_to']);
+                    }
 
                     Flash::notice(__("Logged in."), oneof($redirect, "/"));
                 }
