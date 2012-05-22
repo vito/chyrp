f5b4dcee3fbebf5305dce2423913842aabd5157d
Tux <tux-amd64@tabuu>
2012-05-22 12:06:47 -0400

    Clean up some other mess (I'd changed the keys to one of my own sets, which I forgot to remove).

--- modules/recaptcha/recaptcha.php
+++ modules/recaptcha/recaptcha.php
@@ -10,18 +10,18 @@
      class ReCaptchaCaptcha implements Captcha {
          static public function getCaptcha() {
             require_once INCLUDES_DIR."/lib/recaptchalib.php";
-            $publickey = "6LeNvdESAAAAANcv1-lPGCDDfcKUI02HSVEUAq3F";
+            $publickey = "6Lf6RsoSAAAAAEqUPsm4icJTg7Ph3mY561zCQ3l3";
             return recaptcha_get_html($publickey);
          }
 
          static public function verifyCaptcha() {
             require_once INCLUDES_DIR."/lib/recaptchalib.php";
-            $privatekey = "6LeNvdESAAAAAFWWO1-uXQZF-1MTp3L9U1P-X6mG";
+            $privatekey = "6Lf6RsoSAAAAAKn-wPxc1kE-DE0M73i206w56HEN";
             $resp = recaptcha_check_answer ($privatekey,
                                  $_SERVER['REMOTE_ADDR'],
                                  $_POST['recaptcha_challenge_field'],
                                  $_POST['recaptcha_response_field']);
-            if (!$resp->is_valid) 
+            if (!$resp->is_valid)
                 return false;
             else
                 return true;
