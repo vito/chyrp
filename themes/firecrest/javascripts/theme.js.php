255ee334ef8e40ba4828fa8c65426c4c25a8af2f
Arian Xhezairi <arian@xhezairi.com>
2012-10-07 15:44:10 +0200

    Added Google Webfonts https support.
    Replaced HTML Link tag with JS for support of both http and https protocols. Closes #124

--- themes/firecrest/javascripts/theme.js.php
+++ themes/firecrest/javascripts/theme.js.php
@@ -53,5 +53,19 @@ $(function(){
             $("<span class=\"sub\"> | "+ $(this).html() +"</span>").appendTo("#debug h5").first();
         })
     }
+
+	WebFontConfig = {
+	  google: { families: [ 'Droid+Serif:400,400italic:latin' ] }
+	};
+	(function() {
+	  var wf = document.createElement('script');
+	  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	  wf.type = 'text/javascript';
+	  wf.async = 'true';
+	  var s = document.getElementsByTagName('script')[0];
+	  s.parentNode.insertBefore(wf, s);
+	})();
+
 })
 <!-- --></script>
