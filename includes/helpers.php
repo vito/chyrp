54deed52bf7bb93088c6b46568b6e8c582d551f8
Arian Xhezairi <arian@xhezairi.com>
2012-12-26 18:22:00 +0100

    Fixed a longtime login issue.
    When using Chyrp with an IP Address.
    The issue was occurring due to a session key not being created.

--- includes/helpers.php
+++ includes/helpers.php
@@ -23,7 +23,7 @@
                                  array("Session", "write"),
                                  array("Session", "destroy"),
                                  array("Session", "gc"));
-        $domain = (substr_count($_SERVER['HTTP_HOST'], ".")) ? preg_replace("/^www\./", ".", $_SERVER['HTTP_HOST']) : "" ;
+        $domain = (substr_count($_SERVER['HTTP_HOST'], ".")) ? preg_replace("/^www\./", ".", $_SERVER['HTTP_HOST']) : $_SERVER['REMOTE_ADDR'] ;
         session_set_cookie_params(60 * 60 * 24 * 30, "/", $domain);
         session_name("ChyrpSession");
         register_shutdown_function("session_write_close");
