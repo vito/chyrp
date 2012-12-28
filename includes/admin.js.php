2d4dd5e65f5813c60c64233f9f49d716fe461f6f
Arian Xhezairi <arian@xhezairi.com>
2012-12-28 02:33:28 +0100

    Fixed #143 Modules wouldn't enable/disable.
    For some weird reasons defining the expected dataType seems to resolve this. This is temporarily until we figure out the real issue.

--- includes/admin.js.php
+++ includes/admin.js.php
@@ -442,7 +442,7 @@ var Extend = {
                     Extend.redraw()
                 }
             })
-        })
+        }, "text")
 
         $(ui.draggable).css({ left: 0, right: 0, top: 0, bottom: 0 }).appendTo(this)
 
