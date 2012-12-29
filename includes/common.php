e1842298a1e09908d0e2711a88ba5fa02094dad5
Arian Xhezairi <arian@xhezairi.com>
2012-12-29 07:49:17 +0100

    Chyrp v2.5 RC1
    …and let's just call this Release Candidate.

--- includes/common.php
+++ includes/common.php
@@ -16,7 +16,7 @@
 
     # Constant: CHYRP_VERSION
     # Chyrp's version number.
-    define('CHYRP_VERSION', "2.5b3");
+    define('CHYRP_VERSION', "2.5rc1");
 
     # Constant: DEBUG
     # Should Chyrp use debugging processes?
