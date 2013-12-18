79e966ed63ffe33b18bfd8517e347d4069203d57
Arian Xhezairi <arian@xhezairi.com>
2013-12-18 21:04:29 +0100

    Complete rewrite of the Extension Manager module.

--- modules/extension_manager/extension_manager.php
+++ modules/extension_manager/extension_manager.php
@@ -1,5 +1,6 @@
 <?php
     class ExtensionManager extends Modules {
+
         public function extend_nav($navs) {
             if (Visitor::current()->group->can("toggle_extensions"))
                 $navs["extend_manager"] = array("title" => __("Extension Manager", "extension_manager"));
@@ -9,160 +10,81 @@
 
         static function admin_context($context) {
             if ($_GET['action'] == "extend_manager") {
-                $extensions = file_get_contents("http://chyrp.net/api/v1/chyrp_extensions.php");
-                $extensions = explode(",", $extensions);
-                array_pop($extensions);
-                $extensions = array_reverse($extensions);
-                $content = "";
-
-                foreach($extensions as $id) {
-                    $info=file_get_contents('http://chyrp.net/api/v1/chyrp_extensions.php?id='.$id);
-                    $name = preg_replace("#\s*.*\[name\](.*?)\[/name\].*\s*#s", '$1', $info);
-                    $description = strip_tags(substr(preg_replace("#\s*.*\[description\](.*?)\[/description\].*\s*#s", '$1', $info),0,128)).'…';
-                    $download = preg_replace("#\s*.*\[download\](.*?)\[/download\].*\s*#s", '$1', $info);
-                    $content.='<div class="extension_manager_item"><h3>'.$name.'</h3>';
-                    $content.='<p>'.$description.'<br/>';
-                    $content.='<a href="?action=newextension&url='.$download.'/">Download to Site</a></p></div>';
+                $types = array('feathers', 'modules', 'themes');
+                
+                foreach ($types as $type) {
+                    $ids = file_get_contents("http://chyrp.net/api/1/extensions.php?type=$type");
+                    $ids = explode(",", $ids);
+                    array_pop($ids);
+                    $ids = array_reverse($ids);
+                    
+                    $content = array();
+                    foreach($ids as $id) {
+                        $info = file_get_contents("http://chyrp.net/api/1/extensions.php?id=$id");
+                        $data = json_decode($info, true);
+                        $content[] = array('name' => $data['name'],
+                                           'description' => $data['description'],
+                                           'version' => $data['version'],
+                                           'download' => $data['download']);
+                    }
+                    
+                    $context["extensions"]["$type"] = $content;
                 }
-                $context["extensions"]["extensions"] = $content;
-                $themes=file_get_contents('http://chyrp.net/api/v1/chyrp_themes.php');
-                $themes=explode(',',$themes);
-                array_pop($themes);
-                $themes=array_reverse($themes);
-                $content='';
-                foreach($themes as $id){
-                    $info=file_get_contents('http://chyrp.net/api/v1/chyrp_themes.php?id='.$id);
-                    $name = preg_replace("#\s*.*\[name\](.*?)\[/name\].*\s*#s", '$1', $info);
-                    $description = strip_tags(substr(preg_replace("#\s*.*\[description\](.*?)\[/description\].*\s*#s", '$1', $info),0,128)).'…';
-                    $download = preg_replace("#\s*.*\[download\](.*?)\[/download\].*\s*#s", '$1', $info);
-                    $content.='<div class="extension_manager_item"><h3>'.$name.'</h3>';
-                    $content.='<p>'.$description.'<br/>';
-                    $content.='<a href="?action=newtheme&url='.$download.'/">Download to Site</a></p></div>';
-                }
-                $context["extensions"]["themes"] = $content;
-                $feathers=file_get_contents('http://chyrp.net/api/v1/chyrp_feathers.php');
-                $feathers=explode(',',$feathers);
-                array_pop($feathers);
-                $feathers=array_reverse($feathers);
-                $content='';
-                foreach($feathers as $id){
-                    $info=file_get_contents('http://chyrp.net/api/v1/chyrp_feathers.php?id='.$id);
-                    $name = preg_replace("#\s*.*\[name\](.*?)\[/name\].*\s*#s", '$1', $info);
-                    $description = strip_tags(substr(preg_replace("#\s*.*\[description\](.*?)\[/description\].*\s*#s", '$1', $info),0,128)).'…';
-                    $download = preg_replace("#\s*.*\[download\](.*?)\[/download\].*\s*#s", '$1', $info);
-                    $content.='<div class="extension_manager_item"><h3>'.$name.'</h3>';
-                    $content.='<p>'.$description.'<br/>';
-                    $content.='<a href="?action=newfeather&url='.$download.'/">Download to Site</a></p></div>';
-                }
-                $context["extensions"]["feathers"] = $content;
             }
             return $context;
         }
 
-        public function route_newextension(){
-        	$fp = fopen ("../modules/latest.zip", 'w+');
-        	$ch = curl_init($_GET['url']); # Here is the file we are downloading
-        	curl_setopt($ch, CURLOPT_FILE, $fp);
-        	curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-        	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        	curl_exec($ch);
-        	curl_close($ch);
-        	fclose($fp);
-        	$zip = new ZipArchive();
-        	$x = $zip->open("../modules/latest.zip");
-        	if ($x == true) {
-        		mkdir("../modules/latest");
-        		$zip->extractTo("../modules/latest");
-        	    $zip->close();
-        		$handle=opendir("../modules/latest");
-				if($handle) {
-					while(($file = readdir($handle)) !== false) {
-						if(is_dir("../modules/latest/".$file)){
-							rename("../modules/latest/".$file,"../modules/".$file);
-						}
-					}
-				}
-				$this->rrmdir("../modules/latest");
-        	    unlink("../modules/latest.zip");
-        	    $this->rrmdir("../modules/__MACOSX");
-        	}
-        	header('location: ?action=extend_manager');
-    	}
+        public function route_makeRequest(){
+            $type = pluralize(strip_tags($_GET['type']));
 
-    	public function route_newtheme(){
-        	$fp = fopen ("../themes/latest.zip", 'w+');
-        	$ch = curl_init($_GET['url']); # Here is the file we are downloading
-        	curl_setopt($ch, CURLOPT_FILE, $fp);
-        	curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-        	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        	curl_exec($ch);
-        	curl_close($ch);
-        	fclose($fp);
-        	$zip = new ZipArchive();
-        	$x = $zip->open("../themes/latest.zip");
-        	if ($x == true) {
-        		mkdir("../themes/latest");
-        		$zip->extractTo("../themes/latest");
-        	    $zip->close();
-        		$handle=opendir("../themes/latest");
-				if ($handle) {
-					while (($file = readdir($handle)) !== false) {
-						if (is_dir("../themes/latest/".$file))
-							rename("../themes/latest/".$file,"../themes/".$file);
-					}
-				}
+            set_time_limit(0);
 
-				$this->rrmdir("../themes/latest");
-        	    unlink("../themes/latest.zip");
-        	    $this->rrmdir("../themes/__MACOSX");
-        	}
+            $fp = fopen("../{$type}/latest.zip", 'w+');
+            $url = str_replace(" ", "%20", strip_tags($_GET['url']));
 
-        	header('location: ?action=extend_manager');
-    	}
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+            curl_setopt($ch, CURLOPT_FILE, $fp); # write curl response to file
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_exec($ch);
+            curl_close($ch);
+            fclose($fp);
 
-    	public function route_newfeather() {
-        	$fp = fopen ("../feathers/latest.zip", 'w+');
-        	$ch = curl_init($_GET['url']); # Here is the file we are downloading
-        	curl_setopt($ch, CURLOPT_FILE, $fp);
-        	curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-        	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        	curl_exec($ch);
-        	curl_close($ch);
-        	fclose($fp);
-        	$zip = new ZipArchive();
-        	$x = $zip->open("../feathers/latest.zip");
-        	if ($x == true) {
-        		mkdir("../feathers/latest");
-        		$zip->extractTo("../feathers/latest");
-        	    $zip->close();
-        		$handle=opendir("../feathers/latest");
-				if($handle) {
-					while(($file = readdir($handle)) !== false) {
-						if(is_dir("../feathers/latest/".$file))
-							rename("../feathers/latest/".$file,"../modules/".$file);
-					}
-				}
+            $zip = new ZipArchive;
+            if ($zip->open("../{$type}/latest.zip") == true) {
+                mkdir("../{$type}/latest", 0777);
+                $zip->extractTo("../{$type}/latest");
+                $zip->close();
+                $handle = opendir("../{$type}/latest");
+                if ($handle) {
+                    while (($file = readdir($handle)) !== false) {
+                        if (is_dir("../{$type}/latest/{$file}")) {
+                            if ($file != '.' and $file != '..') {
+                                rename("../{$type}/latest/{$file}", "../{$type}/{$file}");
+                            }
+                        }
+                    }
+                }
 
-				$this->rrmdir("../feathers/latest");
-        	    unlink("../feathers/latest.zip");
-        	    $this->rrmdir("../feathers/__MACOSX");
-        	    $this->rrmdir("../feathers/__MACOSX");
-        	}
+                $this->rrmdir("../{$type}/latest");
+                unlink("../{$type}/latest.zip");
+                $this->rrmdir("../{$type}/__MACOSX");
+            }
 
-        	header('location: ?action=extend_manager');
-    	}
-
-    	//from http://www.php.net/manual/en/function.rmdir.php#98622
-		function rrmdir($dir) { 
- 			if (is_dir($dir)) { 
- 				$objects = scandir($dir); 
- 				foreach ($objects as $object) { 
- 				  	if ($object != "." && $object != "..") { 
- 				    	if (filetype($dir."/".$object) == "dir") $this->rrmdir($dir."/".$object); else unlink($dir."/".$object); 
- 					} 
- 				} 
- 				reset($objects); 
- 				rmdir($dir); 
- 			} 
- 		} 	
+            Flash::notice(__("Extension downloaded successfully."), "/admin/?action=extend_manager");
+        }
+    
+        // from http://www.php.net/manual/en/function.rmdir.php#98622
+        function rrmdir($dir) { 
+            if (is_dir($dir)) {
+                $objects = scandir($dir);
+                foreach ($objects as $object) {
+                    if ($object != "." && $object != "..") {
+                        if (filetype($dir."/".$object) == "dir") $this->rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                    }
+                }
+                reset($objects);
+                rmdir($dir);
+            }
+        }
     }
